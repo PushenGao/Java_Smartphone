62ec1204f2afed751669fe3e4d35497576aa8cdb
JiateLi <lijiate1992@gmail.com>
2015-04-11 00:02:43 -0700

    add exception handler abstract class to use

---
+++ Application/src/main/java/exception/ExceptionHandler.java
@@ -0,0 +1,11 @@
+package exception;
+
+/**
+ * Created by JiateLi on 15/4/11.
+ */
+public abstract class ExceptionHandler {
+
+    public void fix(){
+
+    }
+}
---
+++ Application/src/main/java/exception/UserIDNotFindExceptionHandler.java
@@ -0,0 +1,12 @@
+package exception;
+
+/**
+ * Created by JiateLi on 15/4/11.
+ */
+public class UserIDNotFindExceptionHandler extends ExceptionHandler{
+
+    public void fix(){
+
+    }
+
+}
