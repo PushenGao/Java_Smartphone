7ad558ed7df275b939c82163a4ef2329e73aa2f4
JiateLi <lijiate1992@gmail.com>
2015-04-11 15:23:55 -0700

    IDAO methods

--- Application/src/main/java/ws/local/AccountDAO.java
+++ Application/src/main/java/ws/local/AccountDAO.java
@@ -1,27 +1,29 @@
 package ws.local;
 
+import model.Account;
+
 /**
  * Created by JiateLi on 15/4/11.
  */
 public class AccountDAO implements IDAO {
 
     @Override
-    public void insertAccount() {
+    public void insertAccount(Account account) {
 
     }
 
     @Override
-    public void deleteAccount() {
+    public void deleteAccount(Account account) {
 
     }
 
     @Override
-    public void updateAccount() {
+    public void updateAccount(Account account) {
 
     }
 
     @Override
-    public void getAccount() {
+    public void getAccount(String userid) {
 
     }
 }
