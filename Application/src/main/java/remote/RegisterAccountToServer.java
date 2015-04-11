5e6a56466e78af6b18d3f48e04c38fce9145ec1c
JiateLi <lijiate1992@gmail.com>
2015-04-11 14:14:20 -0700

    add remote package

---
+++ Application/src/main/java/remote/RegisterAccountToServer.java
@@ -0,0 +1,11 @@
+package remote;
+
+/**
+ * Created by JiateLi on 15/4/11.
+ */
+public class RegisterAccountToServer {
+
+    public void register(){
+
+    }
+}
---
+++ Application/src/main/java/remote/UpdateAccountInfo.java
@@ -0,0 +1,11 @@
+package remote;
+
+/**
+ * Created by JiateLi on 15/4/11.
+ */
+public class UpdateAccountInfo {
+
+    public void updateAccount(){
+
+    }
+}
