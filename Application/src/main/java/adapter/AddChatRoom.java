bf4592e48496c998e46a68bc715c72c60ba9c009
JiateLi <lijiate1992@gmail.com>
2015-04-11 17:27:53 -0700

    add interface and abstract class for CHatroom

---
+++ Application/src/main/java/adapter/AddChatRoom.java
@@ -0,0 +1,10 @@
+package adapter;
+
+import model.ChatRoom;
+
+/**
+ * Created by JiateLi on 15/4/11.
+ */
+public interface AddChatRoom {
+    public void addChatRoom(ChatRoom chatRoom);
+}
---
+++ Application/src/main/java/adapter/BuildChatRoom.java
@@ -0,0 +1,7 @@
+package adapter;
+
+/**
+ * Created by JiateLi on 15/4/11.
+ */
+public class BuildChatRoom extends ChatRoomProxy implements AddChatRoom, DeleteChatRoom, UpdateChatRoom{
+}
