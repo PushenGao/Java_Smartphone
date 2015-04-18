c6eb02fb6cab5b09b294046fc7ae9423b0dca084
JiateLi <lijiate1992@gmail.com>
2015-04-17 23:51:14 -0700

    add new interface for remote

---
+++ Application/src/main/java/ws/remote/SearchAccount.java
@@ -0,0 +1,10 @@
+package ws.remote;
+
+import model.BasicAccount;
+
+/**
+ * Created by JiateLi on 15/4/17.
+ */
+public interface SearchAccount {
+    public BasicAccount searchAccount(String userid);
+}
