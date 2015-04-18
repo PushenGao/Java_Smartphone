cd68d6755d18bcbbf4bf7362ea84abb0f4f2baf5
JiateLi <lijiate1992@gmail.com>
2015-04-17 23:13:29 -0700

    delete useless comments

--- Application/src/main/java/ui/Contact.java
+++ Application/src/main/java/ui/Contact.java
@@ -63,48 +63,18 @@ public class Contact extends ActionBarActivity {
     private List<Account> getData()
     {
         List<Account> list=new ArrayList<Account>();
-//		好友数据共7个好友数据
         Account friend1=new Account();
-        //friend1.setIv_photo(R.drawable.show04);
-        friend1.setName("西西");
-        //friend1.setTv_02("你是我的贝贝，我是你的舒舒");
+        friend1.setName("Nancy");
 
         Account friend2=new Account();
-        //friend2.setIv_photo(R.drawable.show01);
-        friend2.setName("小南");
-        //friend2.setTv_02("我还有什么理由可以回到以前？");
+        friend2.setName("Joe");
 
         Account friend3=new Account();
-        //friend3.setIv_photo(R.drawable.show02);
-        friend3.setName("城池");
-       // friend3.setTv_02("做你坚固的壁垒");
-//
-//        FriendModel friend4=new FriendModel();
-//        friend4.setIv_photo(R.drawable.show03);
-//        friend4.setTv_01("阿德哈");
-//        friend4.setTv_02("嘻嘻哈哈，快快乐乐");
-//
-//        FriendModel friend5=new FriendModel();
-//        friend5.setIv_photo(R.drawable.show05);
-//        friend5.setTv_01("阿斯顿");
-//        friend5.setTv_02("给我一个不伤心的理由");
-//
-//        FriendModel friend6=new FriendModel();
-//        friend6.setIv_photo(R.drawable.show01);
-//        friend6.setTv_01("小南");
-//        friend6.setTv_02("我还有什么理由可以回到以前？");
-//
-//        FriendModel friend7=new FriendModel();
-//        friend7.setIv_photo(R.drawable.show02);
-//        friend7.setTv_01("城池");
-//        friend7.setTv_02("做你坚固的壁垒");
+        friend3.setName("Annie");
+
         list.add(friend1);
         list.add(friend2);
         list.add(friend3);
-//        list.add(friend4);
-//        list.add(friend5);
-//        list.add(friend6);
-//        list.add(friend7);
 
         return list;
     }
